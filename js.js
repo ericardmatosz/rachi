e0622024281119a2c1c95ab08c4f29bc5e903392
Érica Matos <ericardmatosz@gmail.com>
2022-03-21 19:18:01 -0300

    JS

---
+++ js.js
@@ -0,0 +1,7 @@
+function goToGooglePlay() {
+    window.open('https://play.google.com/store?hl=pt_BR&gl=US')
+}
+
+function goToAppStore() {
+    window.open('https://www.apple.com/br/app-store/')
+}
